f1fedf0b5f83b792572bd13d991e6ff2f645ceeb
USE104 <jose.hssilva97@gmail.com>
2022-11-26 14:03:09 -0300

    Add project files.

---
+++ frmSplash.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace Geradorador_de_Senha
+{
+    partial class frmSplash
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.prgCarregando = new System.Windows.Forms.ProgressBar();
+            this.tmrSlapsh = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Enabled = false;
+            this.pictureBox1.Image = global::Geradorador_de_Senha.Properties.Resources._1943_security;
+            this.pictureBox1.Location = new System.Drawing.Point(39, 13);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(376, 256);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // prgCarregando
+            // 
+            this.prgCarregando.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.prgCarregando.Enabled = false;
+            this.prgCarregando.ForeColor = System.Drawing.Color.Transparent;
+            this.prgCarregando.Location = new System.Drawing.Point(71, 304);
+            this.prgCarregando.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.prgCarregando.Name = "prgCarregando";
+            this.prgCarregando.Size = new System.Drawing.Size(297, 58);
+            this.prgCarregando.Step = 1;
+            this.prgCarregando.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.prgCarregando.TabIndex = 1;
+            // 
+            // tmrSlapsh
+            // 
+            this.tmrSlapsh.Tick += new System.EventHandler(this.tmrSlapsh_Tick);
+            // 
+            // frmSplash
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(443, 377);
+            this.ControlBox = false;
+            this.Controls.Add(this.prgCarregando);
+            this.Controls.Add(this.pictureBox1);
+            this.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "frmSplash";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.frmSplash_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ProgressBar prgCarregando;
+        private System.Windows.Forms.Timer tmrSlapsh;
+    }
+}
---
+++ frmgeradorSenha.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace Geradorador_de_Senha
+{
+    partial class frmgeradorSenha
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grpText = new System.Windows.Forms.GroupBox();
+            this.grpButton = new System.Windows.Forms.GroupBox();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnCopiar = new System.Windows.Forms.Button();
+            this.btngerarSenha = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.grpText.SuspendLayout();
+            this.grpButton.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grpText
+            // 
+            this.grpText.Controls.Add(this.pictureBox1);
+            this.grpText.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.grpText.Location = new System.Drawing.Point(53, 54);
+            this.grpText.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.grpText.Name = "grpText";
+            this.grpText.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.grpText.Size = new System.Drawing.Size(399, 234);
+            this.grpText.TabIndex = 0;
+            this.grpText.TabStop = false;
+            // 
+            // grpButton
+            // 
+            this.grpButton.Controls.Add(this.btnLimpar);
+            this.grpButton.Controls.Add(this.btnCopiar);
+            this.grpButton.Controls.Add(this.btngerarSenha);
+            this.grpButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.grpButton.Location = new System.Drawing.Point(111, 335);
+            this.grpButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.grpButton.Name = "grpButton";
+            this.grpButton.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.grpButton.Size = new System.Drawing.Size(279, 146);
+            this.grpButton.TabIndex = 1;
+            this.grpButton.TabStop = false;
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.BackColor = System.Drawing.SystemColors.Info;
+            this.btnLimpar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnLimpar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLimpar.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold);
+            this.btnLimpar.Location = new System.Drawing.Point(8, 12);
+            this.btnLimpar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(132, 57);
+            this.btnLimpar.TabIndex = 1;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = false;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // btnCopiar
+            // 
+            this.btnCopiar.BackColor = System.Drawing.SystemColors.Info;
+            this.btnCopiar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCopiar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCopiar.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCopiar.Location = new System.Drawing.Point(139, 12);
+            this.btnCopiar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnCopiar.Name = "btnCopiar";
+            this.btnCopiar.Size = new System.Drawing.Size(132, 57);
+            this.btnCopiar.TabIndex = 2;
+            this.btnCopiar.Text = "Copiar";
+            this.btnCopiar.UseVisualStyleBackColor = false;
+            this.btnCopiar.Click += new System.EventHandler(this.btnCopiar_Click);
+            // 
+            // btngerarSenha
+            // 
+            this.btngerarSenha.BackColor = System.Drawing.SystemColors.Info;
+            this.btngerarSenha.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btngerarSenha.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btngerarSenha.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btngerarSenha.Location = new System.Drawing.Point(8, 65);
+            this.btngerarSenha.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btngerarSenha.Name = "btngerarSenha";
+            this.btngerarSenha.Size = new System.Drawing.Size(263, 74);
+            this.btngerarSenha.TabIndex = 0;
+            this.btngerarSenha.Text = "Gerar Senha";
+            this.btngerarSenha.UseVisualStyleBackColor = false;
+            this.btngerarSenha.Click += new System.EventHandler(this.btngerarSenha_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Schoolbook", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(69, 11);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(339, 40);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Gerador de Senha";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.txtSenha.Font = new System.Drawing.Font("Arial Rounded MT Bold", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSenha.Location = new System.Drawing.Point(53, 289);
+            this.txtSenha.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtSenha.MaxLength = 24;
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.Size = new System.Drawing.Size(397, 36);
+            this.txtSenha.TabIndex = 1;
+            this.txtSenha.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnSair
+            // 
+            this.btnSair.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.btnSair.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSair.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSair.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSair.Location = new System.Drawing.Point(180, 489);
+            this.btnSair.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(132, 42);
+            this.btnSair.TabIndex = 3;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = false;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Image = global::Geradorador_de_Senha.Properties.Resources.networks;
+            this.pictureBox1.Location = new System.Drawing.Point(8, 12);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(381, 213);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // frmgeradorSenha
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(504, 545);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSenha);
+            this.Controls.Add(this.grpButton);
+            this.Controls.Add(this.grpText);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
+            this.Name = "frmgeradorSenha";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.frmgeradorSenha_Load);
+            this.grpText.ResumeLayout(false);
+            this.grpButton.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grpText;
+        private System.Windows.Forms.GroupBox grpButton;
+        private System.Windows.Forms.TextBox txtSenha;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnCopiar;
+        private System.Windows.Forms.Button btngerarSenha;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnSair;
+    }
+}
+
